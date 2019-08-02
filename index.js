fc6c9d98c5660a3bed1c82a63ade891237115f10
Luis Zavala <luis@motiondisplays.cl>
2019-08-02 19:27:52 -0400

    Update CORS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'localhost:3001');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
