cc9c2a792e688d51d9b0008f49fa9daad214f4ef
Luis Zavala <luis@motiondisplays.cl>
2019-08-03 00:18:53 -0400

    Add pagination in get_products

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const express = require("express");
 // create express application instance
 const app = express();
 const https = require("https");
-var redis = require("redis");
-var bodyParser = require("body-parser");
-var cors = require('cors')
+const redis = require("redis");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 // create and connect redis client to local instance.
-var client = redis.createClient();
+let client = redis.createClient();
 app.use(express.urlencoded());
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-
+const PRODUCTSSKUS = require("./productsSku");
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
   next();
-}
+};
 app.use(allowCrossDomain);
-
 
 client.on("error", err => {
   console.log("Error " + err);
@@ -65,21 +67,16 @@ app.post("/get_product_by_id", function(req, res) {
   // });
 });
 
-app.post("/get_products", function(req, res) {
-  console.log("Params: ", req.body.ids);
+app.post("/get_products", function(req, res, next) {
   const productsIds = req.body.ids;
+  const offset = req.body.offset;
+  const limit = req.body.limit;
   const productsRedisKey = "products";
+  const ProductSkuCopy = PRODUCTSSKUS.slice(0);
+  const ProductSkuFilter = ProductSkuCopy.splice(offset, limit + offset);
 
-  // client.get(productsRedisKey, (err, products) => {
-  //   console.log(products.toString());
-  // });
-
-  if (productsIds !== undefined) {
-    url =
-      "https://simple.ripley.cl/api/v2/products?partNumbers=" +
-      productsIds.toString();
-
-    https
+  function fetch_retry(url) {
+    let request = https
       .get(url, resp => {
         let data = "";
         resp.on("data", chunk => {
@@ -116,12 +113,25 @@ app.post("/get_products", function(req, res) {
       .on("error", err => {
         console.log("Error: " + err.message);
       });
+    request.end();
+  }
+
+  if (ProductSkuFilter.length) {
+    url =
+      "https://simple.ripley.cl/api/v2/products?partNumbers=" +
+      ProductSkuFilter.toString();
+
+    const perc = Math.random(0) * 100;
+    if (perc > 10) {
+      fetch_retry(url);
+    } else {
+      res.status(500).json({ error: "Problema interno" });
+    }
   } else {
-    res.status(500).json({ error: "Debe enviar los sku" });
+    res.status(200).json({});
   }
 });
 
 app.listen(8080, () => {
   console.log("El servidor está inicializado en el puerto 3000");
 });
-
